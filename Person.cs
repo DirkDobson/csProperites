e72ee72bcf42deeb887baf574e5d11b7aceba59b
Dirk Dobson <dirkvancouver@live.com>
2019-01-29 18:47:34 -0700

    finshed

--- Person.cs
+++ Person.cs
@@ -4,8 +4,14 @@ namespace Properites
 {
 public class Person
     {
-        public DateTime Birthdate { get; private set; }
+      public string Name { get; set; }
+      public string Username { get; set; }
+       public DateTime Birthdate { get; private set; }
 
+      public Person(DateTime birthdate)
+      {
+          Birthdate = birthdate;
+      }
         public int Age
         {
             get
--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace Properites
         static void Main(string[] args)
         {
             var person = new Person();
-            person.Birthdate = new DateTime( 2013, 1, 1);
             System.Console.WriteLine(person.Age);
         }
     }
